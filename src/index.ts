0b6e249ed606b01b005bb90e109e6e6cb24ab60e
superblocksadmin <admin@superblockshq.com>
2022-07-12 23:36:14 +0000

    Releasing email version 0.1747.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import fs from 'fs';
 import { AttachmentData } from '@sendgrid/helpers/classes/attachment';
 import { MailDataRequired } from '@sendgrid/helpers/classes/mail';
 import sgMail, { MailService } from '@sendgrid/mail';
@@ -11,6 +10,7 @@ import {
   EmailActionFieldNames,
   EmailActionFieldsMap,
   EmailDatasourceConfiguration,
+  ExecutionContext,
   ExecutionOutput,
   FormItem,
   IntegrationError,
@@ -19,11 +19,12 @@ import {
   NotFoundError,
   RawRequest
 } from '@superblocksteam/shared';
-import { BasePlugin, PluginExecutionProps, RequestFile, RequestFiles } from '@superblocksteam/shared-backend';
+import { BasePlugin, PluginExecutionProps, RequestFile, RequestFiles, getEncodedFile } from '@superblocksteam/shared-backend';
 import { isEmpty } from 'lodash';
 
 export default class EmailPlugin extends BasePlugin {
   async execute({
+    context,
     datasourceConfiguration,
     actionConfiguration,
     files
@@ -36,7 +37,7 @@ export default class EmailPlugin extends BasePlugin {
       }
     }
 
-    const msg = this.formEmailJson(actionConfiguration, files);
+    const msg = await this.formEmailJson(context, actionConfiguration, files);
 
     try {
       const client = this.createClient(datasourceConfiguration);
@@ -70,7 +71,11 @@ export default class EmailPlugin extends BasePlugin {
     return Object.values(EmailActionFieldNames);
   }
 
-  formEmailJson(actionConfiguration: EmailActionConfiguration, files: RequestFiles = undefined): MailDataRequired {
+  async formEmailJson(
+    context: ExecutionContext,
+    actionConfiguration: EmailActionConfiguration,
+    files: RequestFiles = undefined
+  ): Promise<MailDataRequired> {
     let attachments: AttachmentData[];
     if (actionConfiguration.emailAttachments) {
       if (typeof actionConfiguration.emailAttachments === 'string') {
@@ -85,26 +90,36 @@ export default class EmailPlugin extends BasePlugin {
         throw new IntegrationError(`Attachments must be provided in the form of an array of JSON objects.`);
       }
 
-      attachments = actionConfiguration.emailAttachments.map((file: unknown) => {
-        // Check if the object being passed is a Superblocks file
-        // object or has properties that allow it to be read as one
-        if (!isReadableFile(file)) {
-          if (isReadableFileConstructor(file)) {
-            // Sendgrid requires the attached file content to be base64 encoded
-            return { filename: file.name, content: Buffer.from(file.contents).toString('base64'), type: file.type } as AttachmentData;
+      attachments = await Promise.all(
+        actionConfiguration.emailAttachments.map(async (file: unknown) => {
+          // Check if the object being passed is a Superblocks file
+          // object or has properties that allow it to be read as one
+          if (!isReadableFile(file)) {
+            if (isReadableFileConstructor(file)) {
+              // Sendgrid requires the attached file content to be base64 encoded
+              return { filename: file.name, content: Buffer.from(file.contents).toString('base64'), type: file.type } as AttachmentData;
+            }
+
+            throw new IntegrationError(
+              'Cannot read attachments. Attachments can either be Superblocks files or { name: string; contents: string, type: string }.'
+            );
           }
 
-          throw new IntegrationError(
-            'Cannot read attachments. Attachments can either be Superblocks files or { name: string; contents: string, type: string }.'
-          );
-        }
-
-        const match = (files as Array<RequestFile>).find((f) => f.filename === file.$superblocksId);
-        if (!match) {
-          throw new IntegrationError(`Could not locate contents for attachment file ${file.name}`);
-        }
-        return { filename: file.name, content: fs.readFileSync(match.path).toString('base64'), type: file.type } as AttachmentData;
-      });
+          const match = (files as Array<RequestFile>).find((f) => f.filename.startsWith(`${file.$superblocksId}_`));
+          if (!match) {
+            throw new IntegrationError(`Could not locate contents for attachment file ${file.name}`);
+          }
+          try {
+            return {
+              filename: file.name,
+              content: await getEncodedFile(context, match.path, 'base64'),
+              type: file.type
+            } as AttachmentData;
+          } catch (_) {
+            throw new IntegrationError(`Could not retrieve file ${file.name} from controller.`);
+          }
+        })
+      );
     }
 
     return {
@@ -123,7 +138,21 @@ export default class EmailPlugin extends BasePlugin {
     datasourceConfiguration: DatasourceConfiguration,
     files: RequestFiles
   ): RawRequest {
-    return JSON.stringify(this.formEmailJson(actionConfiguration, files), null, 2);
+    // Can't call formEmailJson anymore because it's async and because
+    // we probs shouln't be grabbing the attachment data anyways.
+    // We'll need to add some attachment metadata back in the future.
+    return JSON.stringify(
+      {
+        from: actionConfiguration.emailFrom,
+        to: this.parseEmailAddresses(actionConfiguration.emailTo),
+        cc: this.parseEmailAddresses(actionConfiguration.emailCc),
+        bcc: this.parseEmailAddresses(actionConfiguration.emailBcc),
+        subject: actionConfiguration.emailSubject,
+        html: actionConfiguration.emailBody
+      },
+      null,
+      2
+    );
   }
 
   parseEmailAddresses(emailsString: string): string[] {
